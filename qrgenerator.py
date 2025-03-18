b6db4caed44c65af06b8ba6d82b96544f9657c24
selfxiron <selfxiron@gmail.com>
2025-03-19 02:45:49 +0530

    testing vcard qr code in test_branch

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,14 +1,27 @@
 import os
 import qrcode
 
-data = input("Enter the text or URL: ")
+# vCard data
+vCard_data = """
+BEGIN:VCARD
+VERSION:3.0
+N:Jeet Pratap Singh; Rajput;;;
+FN: Jeet Pratap Singh Rajput
+TITLE: Mechanical Engineering Undergrad
+TEL: +91 8770710960
+EMAIL: selfxiron@gmail.com
+URL: https://www.linkedin.com/in/jeet-pratap-singh-rajput/
+END:CARD
+"""
+
+
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=2
 )
-qr.add_data(data)
+qr.add_data(vCard_data)
 qr.make(fit=True)
 qr_image = qr.make_image(fill="black", back_color="white")
 filename = input("Enter filename to save QR code (without extension): ") + ".png"
